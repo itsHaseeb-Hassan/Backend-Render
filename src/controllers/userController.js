dcac252ccad9328cfea47dc6474e335ff7ab169a
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-29 17:11:43 +0500

    some changes added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,80 +2,74 @@ import User from '../models/userModel.js';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {cloudinary} from '../config/cloundinary/cloudinary.js'
-import sendEmail from '../utils/sendEmail.js';
+import {cloudinary} from '../config/cloundinary/cloudinary.js';
 import crypto from 'crypto';
+import sendEmail from '../utils/sendEmail.js';
+import dotenv from 'dotenv';
 
 
+dotenv.config();
 const createUser = async (req, res, next) => {
     const { name, email, password } = req.body;
-    console.log("req.body", req.files);
     const profileImage = req.file;
 
-    console.log(name, email, password, profileImage);
+    console.log("Received input:", name, email, password, profileImage);
 
     if (!name || !email || !password || !profileImage) {
-        res.send({ status: 400, message: "All Fields Are Required" });
         return next(createHttpError(400, "All Fields Are Required"));
     }
 
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            res.send({ status: 400, message: "User Already Exists" });
             return next(createHttpError(400, "User Already Exists"));
         }
 
         const filePath = profileImage.path;
         let result;
         try {
-            result=await cloudinary.uploader.upload(filePath, {
+            result = await cloudinary.uploader.upload(filePath, {
                 folder: "users",
-            }
-            )
+            });
         } catch (error) {
-            res.send({ status: 500, message: "Image Upload Failed" });
+            console.error('Image Upload Error:', error);
             return next(createHttpError(500, "Image Upload Failed"));
         }
 
         const verificationToken = crypto.randomBytes(32).toString("hex");
         console.log("verificationToken", verificationToken);
 
-        console.log("verificationToken", verificationToken);
+        console.log("verificationToken", verificationToken );
 
         try {
             await sendEmail(email, verificationToken);
+            console.log("Email sent successfully");
         } catch (emailError) {
             console.error('Email Sending Error:', emailError);
-            res.send({ status: 500, message: "Email Sending Failed" });
             return next(createHttpError(500, "Email Sending Failed"));
         }
 
-            try {
-                const hashedPassword = await bcrypt.hash(password, 10);
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
 
-                const newUser = await User.create({
-                    name,
-                    email,
-                    password: hashedPassword,
-                    profileImage: result.secure_url,
-                    verificationToken,
-                    isVerified: false,
-                });
+            const newUser = await User.create({
+                name,
+                email,
+                password: hashedPassword,
+                profileImage: result.secure_url,
+                verificationToken,
+                isVerified: false
+            });
 
-                // const accessToken= jwt.sign({id:newUser._id},process.env.JWT_SECRET_ACCESS,{expiresIn:'2h'})
-    // const refreshToken= jwt.sign({id:newUser._id},process.env.JWT_SECRET_REFRESH,{expiresIn:'1d'})
-    // await newUser.updateOne({refreshToken})
-    console.log(newUser._id)
-    res.status(200).json({id:newUser._id,message: "User Created" });
-            } catch (hashError) {
-                console.error('User creation error:', hashError);
-                res.send({ status: 500, message: "User Creation Failed" });
-                return next(createHttpError(500, "User Creation Failed"));
-            }
+            console.log("New user created successfully:", newUser);
+
+            res.status(200).json({ id: newUser._id });
+        } catch (hashError) {
+            console.error('User Creation Error:', hashError);
+            return next(createHttpError(500, "User Creation Failed"));
+        }
     } catch (error) {
-        console.error('User creation error:', error);
-        res.send({ status: 500, message: "User Creation Failed" });
+        console.error('User Creation Error:', error);
         return next(createHttpError(500, "User Creation Failed"));
     }
 };
@@ -86,14 +80,11 @@ const loginUser = async (req, res, next) => {
     try {
         const existingUser = await User.findOne({ email });
         if (!existingUser) {
-            res.send({ status: 400, message: "User Does Not Exist" });
-            return next(createHttpError(400, "User Does Not Exist"));
-            // res.status(400).json({ message: "User Does Not Exist" });
+            return res.status(400).json({ message: "User Does Not Exist" });
         }
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect) {
-            res.send({ status: 400, message: "Wrong Password" });
             return next(createHttpError(400, "Wrong Password"));
         }
 
@@ -102,15 +93,13 @@ const loginUser = async (req, res, next) => {
 
         await existingUser.updateOne({ refreshToken });
 
-        res.status(200).json({message: "User successfully logged in",  id: existingUser._id, name: existingUser.name, profileImage: existingUser.profileImage, accessToken, refreshToken, isLoggedIn: true, status: 200 });
+        res.status(200).json({ id: existingUser._id, name: existingUser.name, profileImage: existingUser.profileImage, accessToken, refreshToken, isLoggedIn: true, isVerified: existingUser.isVerified, status: 200 });
     } catch (error) {
-        console.error('Login error:', error);
-        res.send({ status: 500, message: "Login Failed" });
+        console.error('Login Error:', error);
         return next(createHttpError(500, "Login Failed"));
     }
 };
-
-const verifyEmail = async (req, res) => {
+ const verifyEmail = async (req, res) => {
     try {
         const token = req.params.token;
         const user = await User.findOne({ verificationToken: token });
@@ -130,4 +119,4 @@ const verifyEmail = async (req, res) => {
     }
 };
 
-export { createUser, loginUser ,verifyEmail};
+export { createUser, loginUser, verifyEmail };
